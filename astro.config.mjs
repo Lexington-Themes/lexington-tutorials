d1874e0a69dabe3c0981387b1f47cd08c2a8e925
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-03-07 08:49:28 +0200

    added: multicolumn layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import devtoolBreakpoints from "astro-devtool-breakpoints";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), ]
+  integrations: [tailwind(), sitemap(), devtoolBreakpoints()]
 });
