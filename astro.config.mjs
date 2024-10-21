6b24c64f4cc4fda408d24e9bb035953793d1347b
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-10-22 01:42:59 +0300

    moved to tailwind v4 and astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
+import tailwindcss from '@tailwindcss/vite';
 import devtoolBreakpoints from "astro-devtool-breakpoints";
-
-
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), devtoolBreakpoints()]
+   vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://tutorials.lexingtonthemes.com',
+  integrations: [ sitemap(), devtoolBreakpoints()]
 });
