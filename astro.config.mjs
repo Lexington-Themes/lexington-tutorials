03b032031f5683eb386f7f7418ef36760d10e0e6
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-03-11 23:35:02 +0200

    better landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
-import devtoolBreakpoints from "astro-devtool-breakpoints";
 export default defineConfig({
    vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://tutorials.lexingtonthemes.com',
-  integrations: [ sitemap(), devtoolBreakpoints()]
+  integrations: [ sitemap()]
 });
